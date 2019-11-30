a2f33b50912abd4fd3c70401e65f3f13a2e7b91b
Mariusz Dabrowski <wayrse@gmail.com>
2019-11-30 15:12:35 -0500

    Eye tracking

--- js/app.js
+++ js/app.js
@@ -38,3 +38,29 @@ earWiggle.to('.ear-right', {duration: 0.1, rotation: 45});
 earWiggle.to('.ear-right', {duration: 0.1, rotation: 0});
 
 window.setInterval(() => earWiggle.play(0), 2500);
+
+// ------------
+// Eye tracking
+// ------------
+
+const eyeRightPupil = document.querySelector('.eye-right-pupil');
+const eyeLeftPupil = document.querySelector('.eye-left-pupil');
+const eyeLeftInner = document.querySelector('.eye-left-inner');
+const innerEyeWidth = eyeLeftInner.getBoundingClientRect().width;
+const innerEyeHeight = eyeLeftInner.getBoundingClientRect().height;
+const pupilWidth = eyeLeftPupil.getBoundingClientRect().width;
+const pupilHeight = eyeLeftPupil.getBoundingClientRect().height;
+const xMovement = (innerEyeWidth - pupilWidth)/2;
+const yMovement = (innerEyeHeight - pupilHeight)/2;
+
+window.addEventListener('mousemove', updateEyePosition);
+
+function updateEyePosition(e) {
+  const mousePercentX = e.clientX / document.body.clientWidth;
+  const mousePercentY = e.clientY / document.body.clientHeight;
+  const posX = (mousePercentX * 2 - 1) * xMovement;
+  const posY = (mousePercentY * 2 - 1) * yMovement;
+
+  eyeLeftPupil.style.transform = `translate(${posX}px, ${posY}px)`;
+  eyeRightPupil.style.transform = `translate(${posX}px, ${posY}px)`;
+}
